50b81e5b8fcc2e968f5dd5e61717f5644de78163
Hakan <hakantekir2002@outlook.com>
2023-04-08 18:45:15 +0300

    feat: Create configure UI function

--- RickAndMortyApp/Scene/CharacterDetails/View/CharacterDetailsViewController.swift
+++ RickAndMortyApp/Scene/CharacterDetails/View/CharacterDetailsViewController.swift
@@ -9,10 +9,40 @@ import UIKit
 
 class CharacterDetailsViewController: UIViewController, Storyboarded {
 
+    @IBOutlet private weak var characterImage: UIImageView!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var specyLabel: UILabel!
+    @IBOutlet weak var genderLabel: UILabel!
+    @IBOutlet weak var originLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var episodesLabel: UILabel!
+    @IBOutlet weak var createdLabel: UILabel!
+    
     var viewModel: CharacterDetailsViewModel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configureUI()
+    }
+    
+    fileprivate func configureUI(){
+        let character = viewModel?.character
+        let episodes = character?.episode?.map { $0.components(separatedBy: "/").last }.compactMap { $0 }
+        let episodesString = episodes?.map{String($0)}.joined(separator: ",")
+        let dateFormatter = DateFormatter()
+        var formattedDate = ""
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        if let date = dateFormatter.date(from: character?.created ?? "") {
+            dateFormatter.dateFormat = "MMM d, yyyy 'at' h:mm a"
+            formattedDate = dateFormatter.string(from: date)
+        }
+        characterImage.loadURL(url: character?.imagePath ?? "", placeholder: UIImage(named: "placeholder"))
+        statusLabel.text = character?.status
+        specyLabel.text = character?.species
+        genderLabel.text = character?.gender
+        originLabel.text = character?.origin?.name
+        locationLabel.text = character?.location?.name
+        episodesLabel.text = episodesString
+        createdLabel.text = formattedDate
     }
 }
