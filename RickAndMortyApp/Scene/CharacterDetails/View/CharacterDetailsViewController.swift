36231f3540d16b1980e4bf21ab243b2dca4756d8
Hakan <hakantekir2002@outlook.com>
2023-04-08 17:17:48 +0300

    feat: Create character details View Controller, View Model and Coordinator

---
+++ RickAndMortyApp/Scene/CharacterDetails/View/CharacterDetailsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CharacterDetailsViewController.swift
+//  RickAndMortyApp
+//
+//  Created by Hakan Tekir on 8.04.2023.
+//
+
+import UIKit
+
+class CharacterDetailsViewController: UIViewController, Storyboarded {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ RickAndMortyApp/Scene/CharacterDetails/ViewModel/CharacterDetailsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  CharacterDetailsViewModel.swift
+//  RickAndMortyApp
+//
+//  Created by Hakan Tekir on 8.04.2023.
+//
+
+import Foundation
+
+class CharacterDetailsViewModel {
+    
+}
