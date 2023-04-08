6329fb0997b8d381dc99f11be53f391e29caaa04
Hakan <hakantekir2002@outlook.com>
2023-04-08 22:35:53 +0300

    feat: Add animation to launch screen

---
+++ RickAndMortyApp/Scene/LaunchScreen/View/LaunchViewController.swift
@@ -0,0 +1,68 @@
+//
+//  LaunchViewController.swift
+//  RickAndMortyApp
+//
+//  Created by Hakan Tekir on 8.04.2023.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+    
+    var coordinator: LaunchCoordinator?
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 300, height: 150))
+        imageView.image = UIImage(named: "logo")
+        return imageView
+    }()
+    
+    private let label: UILabel = {
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
+        label.font = UIFont(name: "Avenir", size: 30)
+        label.textAlignment = .center
+        if UserDefaults.standard.bool(forKey: "First Launch") {
+            label.text = "Welcome"
+        } else {
+            label.text = "Hello"
+        }
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(imageView)
+        view.addSubview(label)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        imageView.center = view.center
+        label.center = CGPoint(x: view.center.x, y: view.center.y * 1.2 + imageView.frame.height)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now()+1.5, execute: {
+            self.animate()
+        })
+    }
+    private func animate(){
+        UIView.animate(withDuration: 1.5, animations: {
+            let sizeX = self.view.frame.size.width * 5
+            let sizeY = sizeX / 2
+            let diffX = sizeX - self.view.frame.size.width
+            let diffY = self.view.frame.size.height - sizeY
+            self.imageView.frame = CGRect(x: -(diffX/2),
+                                          y: diffY/2,
+                                          width: sizeX,
+                                          height: sizeY)
+        })
+        UIView.animate(withDuration: 1, animations: {
+            self.imageView.alpha = 0
+        }) { done in
+            if done {
+                self.coordinator?.showHome()
+            }
+        }
+    }
+}
+
